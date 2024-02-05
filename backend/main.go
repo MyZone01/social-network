0a274d552d14c8b88d1cb1dc74283dceb48e9366
serignmbaye <barsaliou1998@gmail.com>
2024-02-05 20:01:10 +0000

    feat: init models

--- backend/main.go
+++ backend/main.go
@@ -6,13 +6,12 @@ import (
 )
 
 func main() {
-	//intialisation of the backend application
+	//initialisation of the backend application
 	app := octopus.New()
 	// lunch all handlers
-	handlers.HandleAll(app)	
+	handlers.HandleAll(app)
 	if err := app.Run(":8081"); err != nil {
 		panic(err)
 	}
 
 }
-
