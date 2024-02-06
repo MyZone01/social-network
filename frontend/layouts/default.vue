724c10d0c60d78fee7828b2e7f9d402ede5c2aa7
serignmbaye <barsaliou1998@gmail.com>
2024-02-06 15:13:55 +0000

    feat(front): add footer component

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,6 +1,9 @@
 <template>
-  <Body class="dark:bg-gray-900 bg-white">
+  <Body class="dark:bg-gray-900 bg-white overflow-x-hidden">
     <Header />
-    <slot />
+    <main class="flex flex-col w-screen h-screen mt-[--m-top]">
+      <slot />
+    </main>
+    <Footer />
   </Body>
 </template>
