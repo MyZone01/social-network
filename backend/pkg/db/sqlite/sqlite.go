86de7aeae5c748c3926d370f3c8d7915f7d000e2
shaykhouna <shaykhouna66@gmail.com>
2024-02-07 19:40:22 +0000

    Features Migrate logic implemented -Only Up for now

--- backend/pkg/db/sqlite/sqlite.go
+++ backend/pkg/db/sqlite/sqlite.go
@@ -2,9 +2,13 @@ package sqlite
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"os"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/sqlite"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -15,7 +19,7 @@ func OpenDB() *sql.DB {
 	if err != nil {
 		log.Println(err)
 	}
-	
+
 	if errorNoFile != nil {
 		sqlCode, ERR := os.ReadFile("./pkg/db/sqlite/init.sql")
 		if ERR != nil {
@@ -25,6 +29,23 @@ func OpenDB() *sql.DB {
 		if erp != nil {
 			log.Fatal(erp)
 		}
+	}
+
+	migrationDir := "/home/shaykh/Projects/Group_Proj/social-network/backend/pkg/db/migrations/"
+	databasePath := "/home/shaykh/Projects/Group_Proj/social-network/backend/pkg/db/sqlite/social-network.db"
+	m, err := migrate.New("file://"+migrationDir, "sqlite://"+databasePath)
+	if err != nil {
+		fmt.Println(err)
+		// log.Fatal(err)
+	}
+
+	err = m.Up()
+	if err != nil && err != migrate.ErrNoChange {
+		log.Fatal(err)
+	}
+
+	if err, erro := m.Close(); err != nil || erro != nil {
+		log.Fatal(err, erro)
 	}
 
 	return DB
