b893f7bd9966fd8bea1e77395b5bcddfe11dd17e
shaykhouna <shaykhouna66@gmail.com>
2024-02-08 12:36:01 +0000

    fixing migrate writing process

--- backend/pkg/db/sqlite/sqlite.go
+++ backend/pkg/db/sqlite/sqlite.go
@@ -2,17 +2,20 @@ package sqlite
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"os"
 
-	"github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/database/sqlite"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func OpenDB() *sql.DB {
+type Migrations struct {
+	Migration bool
+	Action    string
+	Target    bool
+	Version   int
+}
+
+func OpenDB(migration Migrations) *sql.DB {
 	_, errorNoFile := os.Stat("./pkg/db/sqlite/social-network.db")
 
 	DB, err := sql.Open("sqlite3", "./pkg/db/sqlite/social-network.db")
@@ -31,26 +34,8 @@ func OpenDB() *sql.DB {
 		}
 	}
 
-	currentDir, err := os.Getwd()
-
-	migrationDir := currentDir + "/pkg/db/migrations/sqlite/"
-	fmt.Println(migrationDir)
-	databasePath := currentDir + "/pkg/db/sqlite/social-network.db"
-	m, err := migrate.New("file://"+migrationDir, "sqlite://"+databasePath)
-	if err != nil {
-		fmt.Println(err)
-		// log.Fatal(err)
-	}
-	defer m.Close()
-
-	// Apply migrations (Up)
-	// if err := m.Step(1); err != nil && err != migrate.ErrNoChange {
-	//     log.Fatal(err)
-	// }
-
-	// Rollback one migration (Down)
-	if err := m.Steps(-1); err != nil && err != migrate.ErrNoChange {
-		fmt.Println(err)
+	if migration.Migration {
+		Migration(DB, migration)
 	}
 
 	return DB
