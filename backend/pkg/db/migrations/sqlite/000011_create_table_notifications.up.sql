04861d5a74bab21da51fb048a10b7c9749debdb5
shaykhouna <shaykhouna66@gmail.com>
2024-02-08 17:59:56 +0000

    FIX sql command

--- backend/pkg/db/migrations/sqlite/000011_create_table_notifications.up.sql
+++ backend/pkg/db/migrations/sqlite/000011_create_table_notifications.up.sql
@@ -4,6 +4,5 @@ CREATE TABLE IF NOT EXISTS Notifications (
     type TEXT CHECK(type IN ('follow_request', 'group_invitation', 'new_message', 'new_event')),
     message TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP
 );
