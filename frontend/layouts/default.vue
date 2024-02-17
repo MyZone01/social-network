6865ca754a910dc0eb67b4ff2b9a80823eea05f8
mohamedsarr0 <hoodboibeats@gmail.com>
2024-02-17 01:42:45 +0000

    init profile.vue

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -2,7 +2,7 @@
   <Body class="bg-15-23-42 overflow-x-hidden">
     <Header />
     <Sidebar />
-    <main id="site__main" class="ml-48 2xl:ml-0 py-5 px-6 h-[calc(100vh-var(--m-top))] mt-[--m-top]">
+    <main id="site__main" class="ml-48 2xl:ml-0 py-5 px-6 h-[calc(100vh-var(--m-top))] mt-[--m-top] overflow-y-auto">
       <slot />
     </main>
     <Footer />
