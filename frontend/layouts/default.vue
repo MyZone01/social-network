61af4672ac5135b2a5863b0d6b9c5074743e1834
shaykhouna <shaykhouna66@gmail.com>
2024-02-20 15:51:56 +0000

    adjusting CSS and Layout/Main logic

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -2,7 +2,9 @@
   <Body class="bg-15-23-42 overflow-x-hidden">
     <Header />
     <Sidebar />
+    <section style="max-height: 100%; overflow-y: auto;">
       <slot />
+    </section>
     <Footer />
   </Body>
 </template>
