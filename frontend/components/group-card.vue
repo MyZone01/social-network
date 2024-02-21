ee9f20bb4c03f00065129d37c7d76e300db24b60
bcoulibal <coulou789@gmail.com>
2024-02-21 15:03:58 +0000

    add: group timeline page

--- frontend/components/group-card.vue
+++ frontend/components/group-card.vue
@@ -1,25 +1,27 @@
 <template>
-  <UCard class="bg-gray-100 dark:bg-gray-950 w-72" :ui="{ header: { padding: 'p-0 rounded-t-lg overflow-hidden' } }">
-    <template #header>
-      <img src="assets/images/post/img-2.jpg" class="" alt="">
-    </template>
-    <div class="relative">
-      <UAvatar size="sm" src="https://avatars.githubusercontent.com/u/739984?v=4" alt="Avatar" class="absolute -top-9" />
-      <a href="timeline-event.html">
-        <h4 class="card-title text-2xl font-bold"> Delicious Foods </h4>
-      </a>
+  <UCard class="card bg-gray-100 dark:bg-gray-950 w-72" :ui="{ header: { padding: 'p-0 rounded-t-lg overflow-hidden' } }">
+    <NuxtLink to="/groups/id">
+      <div class="card-media h-24 rounded-t-lg">
+        <img src="assets/images/post/img-2.jpg" class="" alt="">
+      </div>
+    </NuxtLink>
+    <div class="card-body z-10 relative w-full">
+      <UAvatar size="sm" src="https://avatars.githubusercontent.com/u/739984?v=4" alt="Avatar"
+        class="w-10 rounded-full mb-2 shadow -mt-8 relative border-2 border-white dark:border-slate-800" />
+      <NuxtLink to="/groups/id">
+        <h4 class="card-title text-xl font-bold"> Delicious Foods </h4>
+      </NuxtLink>
       <div class="flex mb-4 text-sm mt-2">
         <div>Travel</div>
         <div class="md:block hidden">
-          ·
         </div>
-        <div> 16k members</div>
+        <div> 16k members </div>
       </div>
-      <div class="flex gap-2">
-        <UButton class="items-center justify-center w-10/12">
+      <div class="flex flex-row gap-2 justify-center w-full">
+        <UButton class="items-center bg-blue-500 justify-center flex-1">
           Join
         </UButton>
-        <UButton class="items-center justify-center w-2/12" color="gray">
+        <UButton class="items-center justify-center" color="gray">
           View
         </UButton>
       </div>
---
+++ frontend/pages/groups/[id].vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtLayout>
+    <main id="site__main"
+      class="2xl:ml-[--w-side]  xl:ml-[--w-side-sm] py-10 p-2.5 h-[calc(100vh-var(--m-top))] mt-[--m-top]">
+      <div class="max-w-[1065] mx-auto">
+
+        
+      </div>
+    </main>
+  </NuxtLayout>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
