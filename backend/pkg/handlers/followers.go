ec6c6d98382d098c7bad004c44b881aba889fb6a
mohamedsarr0 <hoodboibeats@gmail.com>
2024-02-28 21:29:24 +0000

    add post proges in this branch

--- backend/pkg/handlers/followers.go
+++ backend/pkg/handlers/followers.go
@@ -2,7 +2,9 @@ package handlers
 
 import (
 	octopus "backend/app"
+	"backend/pkg/config"
 	"backend/pkg/middleware"
+	"backend/pkg/models"
 	"net/http"
 )
 
@@ -12,6 +14,34 @@ import (
 // After successful operation, you can send a response back to the client using methods like ctx.JSON().
 func handleFollower(ctx *octopus.Context) {
 	// TODO: Implement the follower logic here.
+}
+func handleGetAllFollowersRequest(ctx *octopus.Context) {
+	userUUID, err := config.Sess.Start(ctx).Get(ctx.GetBearerToken())
+	if err != nil {
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
+	userFolowers := models.Followers{}
+	userFolowers.GetAllByFolloweeID(ctx.Db.Conn, userUUID)
+	userFolowersJson := []map[string]interface{}{}
+	for _, follower := range userFolowers {
+		newUser := models.User{}
+		if err := newUser.Get(ctx.Db.Conn, follower.FollowerID); err != nil {
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+		userFolowersJson = append(userFolowersJson,
+			map[string]interface{}{
+				"nickname":  newUser.Nickname,
+				"email":     newUser.Email,
+				"firstname": newUser.FirstName,
+				"lastname":  newUser.LastName,
+				"id":        newUser.ID.String(),
+			},
+		)
+	}
+	ctx.JSON(userFolowersJson)
+
 }
 
 // FollowerHandler defines the structure for handling follower requests.
@@ -25,7 +55,17 @@ var FollowerRoute = route{
 	},
 }
 
+var getAllFollowers = route{
+	path:   "/getAllFollowers",
+	method: http.MethodPost,
+	middlewareAndHandler: []octopus.HandlerFunc{
+		middleware.AuthRequired,      // Middleware to check if the request is authenticated.
+		handleGetAllFollowersRequest, // Handler function to process the follower request.
+	},
+}
+
 func init() {
 	// Register the follower route with the global AllHandler map.
 	AllHandler[FollowerRoute.path] = FollowerRoute
+	AllHandler[getAllFollowers.path] = getAllFollowers
 }
