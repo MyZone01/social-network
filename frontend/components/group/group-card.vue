ceb59fb4688d7dc063ac78b53c45efa8fcc9e8ce
bcoulibal <coulou789@gmail.com>
2024-03-02 05:18:24 +0000

    UPDATE: group listing (views)

--- frontend/components/group/group-card.vue
+++ frontend/components/group/group-card.vue
@@ -3,7 +3,7 @@ const props = defineProps(['group'])
 </script>
 
 <template>
-  <UCard class="card max-w-sm bg-gray-100 dark:bg-gray-950  p-0 flex-1"
+  <UCard class="card w-60 bg-gray-100 dark:bg-gray-950  p-0"
     :ui="{ header: { padding: 'p-0 rounded-t-lg overflow-hidden' } }">
     <NuxtLink :href="'/groups/' + props.group.ID">
       <div class="card-media h-24 rounded-t-lg">
@@ -12,7 +12,7 @@ const props = defineProps(['group'])
     </NuxtLink>
     <div class="card-body z-10 relative w-full">
       <NuxtLink :href="'/groups/' + props.group.ID">
-        <h4 class="card-title text-xl font-bold">
+        <h4 class="card-title text-xl font-bold text-ellipsis w-full">
           {{ props.group.Title }}
         </h4>
       </NuxtLink>
--- frontend/pages/groups/index.vue
+++ frontend/pages/groups/index.vue
@@ -8,7 +8,7 @@ onMounted(async () => {
             Authorization: `Bearer ${store.token}`,
         }
     })
-    data.value = response.groups.slice(0, 4)
+    data.value = response.groups
     console.log(response)
 })
 </script>
@@ -17,43 +17,35 @@ onMounted(async () => {
     <NuxtLayout>
         <main id="site__main"
             class="2xl:ml-[--w-side]  xl:ml-[--w-side-sm] py-10 p-2.5 h-[calc(100vh-var(--m-top))] mt-[--m-top]">
-            <div class="2xl:max-w-[1220px] max-w-[1065px] mx-auto">
-                <div class="page-heading">
+            <div class="2xl:max-w-[1220px] max-w-[1065px] mx-auto h-full">
+                <div class="page-heading flex flex-row justify-between align-baseline flex-wrap">
                     <h1 class="page-title">
                         Groups
                     </h1>
-                    <nav class="nav__underline flex flex-row justify-between align-baseline flex-wrap">
-                        <ul class="group"
-                            uk-switcher="connect: #group-tabs ; animation: uk-animation-slide-right-medium, uk-animation-slide-left-medium">
-                            <li> <a href="#"> Suggestions </a> </li>
-                            <li> <a href="#"> Popular </a> </li>
-                            <li> <a href="#"> My groups </a> </li>
-                        </ul>
-                        <div uk-toggle="target: #create-group-overlay" class="h-fit self-center">
-                            <UButton class="bg-blue-500 h-fit">
-                                <UIcon name="i-heroicons-plus" class="" /><span>New</span>
-                            </UButton>
-                        </div>
-                    </nav>
+                    <div uk-toggle="target: #create-group-overlay" class="h-fit self-center">
+                        <UButton class="bg-blue-500 h-fit">
+                            <UIcon name="i-heroicons-plus" class="" /><span>New</span>
+                        </UButton>
+                    </div>
                 </div>
                 <!-- group list tabs -->
-                <div id="group-tabs" class="uk-switcher">
-                    <!-- card layout 1 -->
-                    <div v-if="data.length" class="flex flex-row gap-2 overflow-hidden">
-                        <GroupCard v-for="group in data" :group="group" />
-                    </div>
-                    <div class="flex w-full flex-row justify-center align-middle" v-else >
-                        <div class="flex flex-col justify-center">
-                           <p class="text-base text-lg">there is no group at the moment</p>
-                           <div uk-toggle="target: #create-group-overlay" class="h-fit self-center">
+                <!-- <div id="group-tabs" class="uk-switcher"> -->
+                <!-- card layout 1 -->
+                <div v-if="data.length" class="flex flex-row flex-wrap overflow-scroll">
+                    <GroupCard v-for="group in data" :group="group" />
+                </div>
+                <div class="flex w-full flex-row justify-center align-middle" v-else>
+                    <div class="flex flex-col justify-center">
+                        <p class="text-base text-lg">there is no group at the moment</p>
+                        <div uk-toggle="target: #create-group-overlay" class="h-fit self-center">
 
-                        <UButton class="bg-blue-500 h-fit">
-                            <UIcon name="i-heroicons-plus" class="" /><span>Create new</span>
-                        </UButton> 
+                            <UButton class="bg-blue-500 h-fit">
+                                <UIcon name="i-heroicons-plus" class="" /><span>Create new</span>
+                            </UButton>
                         </div>
                     </div>
 
-                    </div>
+                    <!-- </div> -->
                 </div>
             </div>
             <GroupCreateGroupModal></GroupCreateGroupModal>
