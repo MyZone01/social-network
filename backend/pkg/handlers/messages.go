349a4babdb63abbea1bf304c0573a8551dcdf94d
Papa Alassane  Ba <pba@noreply.learn.zone01dakar.sn>
2024-03-15 16:25:00 +0000

    Upload files to 'backend/pkg/handlers'

--- backend/pkg/handlers/messages.go
+++ backend/pkg/handlers/messages.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	octopus "backend/app"
 	"backend/pkg/middleware"
+	"backend/pkg/models"
+	"fmt"
 	"net/http"
 )
 
@@ -12,6 +14,47 @@ import (
 // After successful operation, you can send a response back to the client using methods like ctx.JSON().
 func handleMessages(ctx *octopus.Context) {
 	// TODO: Implement the messages logic here.
+}
+func GetUsers(ctx *octopus.Context) {
+
+	var users models.Users
+
+	err1 := users.GetAll(ctx.Db.Conn)
+	if err1 != nil {
+		fmt.Println(err1)
+		// HandleError(ctx.ResponseWriter, http.StatusInternalServerError, "Error getting users : "+err1.Error())
+		return
+	}
+	fmt.Println(users)
+
+	data := map[string]interface{}{
+		"list": users,
+	}
+
+	ctx.JSON(data)
+
+	// HandleError(ctx.ResponseWriter, http.StatusUnauthorized, "No active session")
+
+}
+func GetMessages(ctx *octopus.Context) {
+
+	var messages models.PrivateMessages
+
+	// err1 := messages.GetPrivateMessages(ctx.Db.Conn, receiverId)
+	// if err1 != nil {
+	// 	fmt.Println(err1)
+	// 	// HandleError(ctx.ResponseWriter, http.StatusInternalServerError, "Error getting users : "+err1.Error())
+	// 	return
+	// }
+
+	data := map[string]interface{}{
+		"messages_list": messages,
+	}
+
+	ctx.JSON(data)
+
+	// HandleError(ctx.ResponseWriter, http.StatusUnauthorized, "No active session")
+
 }
 
 // MessagesHandler defines the structure for handling messages requests.
@@ -25,7 +68,16 @@ var messagesRoutes = route{
 	},
 }
 
+var getUsers = route{
+	path:   "/users",
+	method: http.MethodGet,
+	middlewareAndHandler: []octopus.HandlerFunc{
+		middleware.NoAuthRequired, // Middleware to check if the request is authenticated.
+		GetUsers,                  // Handler function to process the messages request.
+	},
+}
+
 func init() {
-	// Register the messages route with the global AllHandler map.
-	AllHandler[messagesRoutes.path] = messagesRoutes
+	// Register the events route with the global AllHandler map.
+	AllHandler[getUsers.path] = getUsers
 }
