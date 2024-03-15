356f201ab3c958cc7e3090de621955c2f62467bd
serignmbaye <barsaliou1998@gmail.com>
2024-03-15 17:22:21 +0000

    feat: add WebSocket handler + page for group chat

--- client/server/utils/session.ts
+++ client/server/utils/session.ts
@@ -1,19 +1,24 @@
-import type { H3Event } from "h3";
-import { User } from "~/types";
+import { H3Event } from "h3";
+import { type User } from "~/types";
 
-export async function getUserFromToken(event: H3Event) {
+export async function getUserFromToken(event: H3Event | string) {
   const config = useRuntimeConfig();
-
-  const cookie = getCookie(event, config.cookieName);
-  if (!cookie) return {user: null, token: null};;
+  let cookie: string;
+  if (typeof event !== "string") {
+    const _cookie = getCookie(event, config.cookieName);
+    if (!_cookie) return { user: null, token: null };
+    cookie = _cookie;
+  } else {
+    cookie = event;
+  }
 
   const unsignedToken = unsign(cookie, config.cookieSecret);
-  if (!unsignedToken) return {user: null, token: null};
+  if (!unsignedToken) return { user: null, token: null };
 
   const token = deserialize(unsignedToken);
 
   try {
-    const response = await $fetch<User>("http://localhost:8081/me", {
+    const response = await $fetch("http://localhost:8081/me", {
       method: "GET",
       headers: {
         Accept: "application/json",
@@ -21,14 +26,16 @@ export async function getUserFromToken(event: H3Event) {
         Authorization: `Bearer ${token.session}`,
       },
     });
-    return { user: response, token: token.session };
+    return { user: JSON.parse(response as string) as User, token: token.session };
   } catch (error) {
-    deleteCookie(event, config.cookieName, {
-      httpOnly: true,
-      path: "/",
-      sameSite: "strict",
-      secure: process.env.NODE_ENV === "production",
-    });
-    return {user: null, token: null};;
+    if (typeof event !== "string") {
+      deleteCookie(event, config.cookieName, {
+        httpOnly: true,
+        path: "/",
+        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production",
+      });
+    }
+    return { user: null, token: null };
   }
 }
