ec1315c5db8a5b84bfcf3dc647ffa277191ced0b
pba <alassanebv23@gmail.com>
2024-03-18 15:32:40 +0000

    redirecting the name of the user to his profil page

--- client/pages/messages.vue
+++ client/pages/messages.vue
@@ -310,75 +310,75 @@
               </button>
             </div>
           </div>
+          <div class="rightt w-full h-full absolute top-0 right-0 z-10 hidden transition-transform">
+            <div
+              class="w-[360px] border-l shadow-lg h-screen bg-white absolute right-0 top-0 uk-animation-slide-right-medium delay-200 z-50 dark:bg-dark2 dark:border-slate-700">
+              <div class="w-full h-1.5 bg-gradient-to-r to-purple-500 via-red-500 from-pink-500 -mt-px" />
+  
+              <div class="py-10 text-center text-sm pt-20">
+                <nuxt-img :src="'http://localhost:8081/' + user.avatarImage" class="w-24 h-24 rounded-full mx-auto mb-3" alt=""></nuxt-img>
+                <div  class="mt-8">
+                  <div class="md:text-xl text-base font-medium text-black dark:text-white">
+                    {{ user.firstName}} {{ user.lastName}}
+                  </div>
+                  <div class="text-gray-500 text-sm mt-1 dark:text-white/80">
+                   
+                  </div>
+                </div>
+                <div class="mt-5">
+                  <a href="/profile/"
+                    class="inline-block rounded-full px-4 py-1.5 text-sm font-semibold bg-secondery">View profile</a>
+                </div>
+              </div>
+  
+              <hr class="opacity-80 dark:border-slate-700">
+  
+              <ul class="text-base font-medium p-3">
+                <li>
+                  <div class="flex items-center gap-5 rounded-md p-3 w-full hover:bg-secondery">
+                    <ion-icon :icon="ioniconsNotificationsOffOutline" class="text-2xl" /> Mute Notification
+                    <label class="switch cursor-pointer ml-auto"> <input type="checkbox" checked><span
+                        class="switch-button !relative" /></label>
+                  </div>
+                </li>
+                <li>
+                  <button type="button" class="flex items-center gap-5 rounded-md p-3 w-full hover:bg-secondery">
+                    <ion-icon :icon="ioniconsFlagOutline" class="text-2xl" /> Report
+                  </button>
+                </li>
+                <li>
+                  <button type="button" class="flex items-center gap-5 rounded-md p-3 w-full hover:bg-secondery">
+                    <ion-icon :icon="ioniconsSettingsOutline" class="text-2xl" /> Ignore messages
+                  </button>
+                </li>
+                <li>
+                  <button type="button" class="flex items-center gap-5 rounded-md p-3 w-full hover:bg-secondery">
+                    <ion-icon :icon="ioniconsStopCircleOutline" class="text-2xl" /> Block
+                  </button>
+                </li>
+                <li>
+                  <button type="button"
+                    class="flex items-center gap-5 rounded-md p-3 w-full hover:bg-red-50 text-red-500">
+                    <ion-icon :icon="ioniconsTrashOutline" class="text-2xl" /> Delete Chat
+                  </button>
+                </li>
+              </ul>
+  
+              <!-- close button -->
+              <button type="button" class="absolute top-0 right-0 m-4 p-2 bg-secondery rounded-full"
+                uk-toggle="target: .rightt ; cls: hidden">
+                <ion-icon :icon="ioniconsClose" class="text-2xl flex" />
+              </button>
+            </div>
+  
+            <!-- overly -->
+            <div class="bg-slate-100/40 backdrop-blur absolute w-full h-full dark:bg-slate-800/40"
+              uk-toggle="target: .rightt ; cls: hidden" />
+          </div>
         </div>
 
 
         <!-- user profile right info -->
-        <div class="rightt w-full h-full absolute top-0 right-0 z-10 hidden transition-transform">
-          <div
-            class="w-[360px] border-l shadow-lg h-screen bg-white absolute right-0 top-0 uk-animation-slide-right-medium delay-200 z-50 dark:bg-dark2 dark:border-slate-700">
-            <div class="w-full h-1.5 bg-gradient-to-r to-purple-500 via-red-500 from-pink-500 -mt-px" />
-
-            <div class="py-10 text-center text-sm pt-20">
-              <img src="../assets/images/avatars/avatar-3.jpg" class="w-24 h-24 rounded-full mx-auto mb-3" alt="">
-              <div class="mt-8">
-                <div class="md:text-xl text-base font-medium text-black dark:text-white">
-                  Monroe Parker
-                </div>
-                <div class="text-gray-500 text-sm mt-1 dark:text-white/80">
-                  @Monroepark
-                </div>
-              </div>
-              <div class="mt-5">
-                <a href="timeline.html"
-                  class="inline-block rounded-full px-4 py-1.5 text-sm font-semibold bg-secondery">View profile</a>
-              </div>
-            </div>
-
-            <hr class="opacity-80 dark:border-slate-700">
-
-            <ul class="text-base font-medium p-3">
-              <li>
-                <div class="flex items-center gap-5 rounded-md p-3 w-full hover:bg-secondery">
-                  <ion-icon :icon="ioniconsNotificationsOffOutline" class="text-2xl" /> Mute Notification
-                  <label class="switch cursor-pointer ml-auto"> <input type="checkbox" checked><span
-                      class="switch-button !relative" /></label>
-                </div>
-              </li>
-              <li>
-                <button type="button" class="flex items-center gap-5 rounded-md p-3 w-full hover:bg-secondery">
-                  <ion-icon :icon="ioniconsFlagOutline" class="text-2xl" /> Report
-                </button>
-              </li>
-              <li>
-                <button type="button" class="flex items-center gap-5 rounded-md p-3 w-full hover:bg-secondery">
-                  <ion-icon :icon="ioniconsSettingsOutline" class="text-2xl" /> Ignore messages
-                </button>
-              </li>
-              <li>
-                <button type="button" class="flex items-center gap-5 rounded-md p-3 w-full hover:bg-secondery">
-                  <ion-icon :icon="ioniconsStopCircleOutline" class="text-2xl" /> Block
-                </button>
-              </li>
-              <li>
-                <button type="button"
-                  class="flex items-center gap-5 rounded-md p-3 w-full hover:bg-red-50 text-red-500">
-                  <ion-icon :icon="ioniconsTrashOutline" class="text-2xl" /> Delete Chat
-                </button>
-              </li>
-            </ul>
-
-            <!-- close button -->
-            <button type="button" class="absolute top-0 right-0 m-4 p-2 bg-secondery rounded-full"
-              uk-toggle="target: .rightt ; cls: hidden">
-              <ion-icon :icon="ioniconsClose" class="text-2xl flex" />
-            </button>
-          </div>
-
-          <!-- overly -->
-          <div class="bg-slate-100/40 backdrop-blur absolute w-full h-full dark:bg-slate-800/40"
-            uk-toggle="target: .rightt ; cls: hidden" />
-        </div>
       </div>
     </div>
   </main>
